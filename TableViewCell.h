41b8d1a9f8aede4996ff0493a8515c59da321387
jsl328 <jsl328@yeah.net>
2015-11-25 00:40:29 +0800

    pp
    pp

---
+++ TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  24Test
+//
+//  Created by jiangshilin on 15/11/25.
+//  Copyright (c) 2015年 jiangshilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end

01260e22b4f17940c7f2014045652bf5c15a0b6f
jsl328 <jsl328@yeah.net>
2015-11-25 13:25:47 +0800

    新建的model文件要提交

---
+++ Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  24Test
+//
+//  Created by jiangshilin on 15/11/25.
+//  Copyright (c) 2015年 jiangshilin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  24Test
+//
+//  Created by jiangshilin on 15/11/25.
+//  Copyright (c) 2015年 jiangshilin. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end

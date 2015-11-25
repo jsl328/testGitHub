b6a2973639091c9fdb2e9c8c552dce5c35d13d12
jsl328 <jsl328@yeah.net>
2015-11-25 13:18:33 +0800

    第二次提交

--- 24Test/ViewController.m
+++ 24Test/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"test flayer");
+    NSLog(@"test flayer2");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
